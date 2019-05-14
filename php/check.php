81fb1582caafb13066dc6361dd975e10cf41c2df
joshianil874 <44560979+joshianil874@users.noreply.github.com>
2019-05-14 12:04:57 -0400

    Update check.php
    request IP and validating URL

--- php/check.php
+++ php/check.php
@@ -2,12 +2,12 @@
 require 'config.phplib';
 
 $msg="";
-	if (isset($_SESSION['user']) === false){
+if (!array_key_exists('hiwa-user', $_COOKIE) ||
+    !array_key_exists('hiwa-role', $_COOKIE)) {
 	Header("Location: login.php");
+	exit();
 	}
-$role=$_SESSION['user'];
-
-
+$role=$_SESSION['hiwa-user'];
 if ($role != 'admin') Header("Location: menu.php");
 ?>
 <html>
@@ -20,6 +20,9 @@ if ($role != 'admin') Header("Location: menu.php");
 <?php require 'header.php';
 
 if (array_key_exists("ip", $_REQUEST)) {
+	$ip=$_REQUEST['ip'];
+	if(filter_var($ip, FILTER_VALIDATION_URL) || filter_var($ip, FILTER_VALIDATION_IP) || filter_var($ip, FILTER_FLAG_DOMAIN, FILTER_FLAG_HOSTNAME))
+	{
 	echo "<P>pinging target IP address</P>";
 	exec("ping -c 3 $_REQUEST[ip]", $out);
 	echo "<div><pre>\r\n";
